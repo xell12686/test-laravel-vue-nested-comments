90dd5a3830b2c10bfc7daefa84c7ecc7fdffecbd
James FL <jlacandula@gmail.com>
2021-04-05 22:54:24 +0800

    comments table migration schema

--- database/migrations/2021_04_05_145138_create_comment_table.php
+++ database/migrations/2021_04_05_145138_create_comment_table.php
@@ -15,7 +15,12 @@ class CreateCommentTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('parent_id')->default(0);
+            $table->text('content');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
